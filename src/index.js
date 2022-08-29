0ba14f9557e463087d8e7b439599b0f1950f674d
MacenaAndre <andremacenacosta@gmail.com>
2022-08-29 05:34:40 -0300

    Post & Get /tweets

--- src/index.js
+++ src/index.js
@@ -12,6 +12,17 @@ server.post("/sign-up", (req, res) => {
     let newUser = req.body; 
     users.push(newUser);
     res.send("OK");
+});
+
+server.post("/tweets", (req, res) => {
+    let newTweet = req.body;
+    tweets.push(newTweet);
+    res.send("OK");
+});
+
+server.get("/tweets", (req, res) => {
+    let visibleTweets = tweets.filter((value, index) => index >= tweets.length -10);
+    res.send(visibleTweets);
 })
 
 server.listen(5000, () => console.log("Listening on port 5000..."))
